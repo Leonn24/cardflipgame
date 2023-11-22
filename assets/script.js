3477ec470f939d5b7469818fb8ca38971fd9f783
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2023-11-22 17:36:25 -0500

    added index/css/JavaScript files and images for card game

---
+++ assets/script.js
@@ -0,0 +1,79 @@
+// Card Array
+
+const cardArray = [
+    {
+        name: 'Cloud',
+        img: 'images/Cloud.jpg',
+    },
+    {
+        name: 'Barett',
+        img: 'images/Barett.jpg',
+    },
+    {
+        name: 'Chocobo',
+        img: 'images/Chocobo.jpg',
+    },
+    {
+        name: 'Seph',
+        img: 'images/Seph.jpg',
+    },
+    {
+        name: 'Red XIII',
+        img: 'images/Red XIII.jpg',
+    },    
+    {
+        name: 'Tifa',
+        img: 'images/Tifa.jpg',
+    },
+    {
+        name: 'Vincent',
+        img: 'images/Vincent.jpg',
+    },
+    {
+        name: 'Yuffie',
+        img: 'images/Yuffie.jpg',
+    },
+    {
+        name: 'Aerith',
+        img: 'images/Aerith.jpg',
+    },
+    {
+        name: 'Cloud',
+        img: 'images/Cloud.jpg',
+    },
+    {
+        name: 'Barett',
+        img: 'images/Barett.jpg',
+    },
+    {
+        name: 'Chocobo',
+        img: 'images/Chocobo.jpg',
+    },
+    {
+        name: 'Seph',
+        img: 'images/Seph.jpg',
+    },
+    {
+        name: 'Red XIII',
+        img: 'images/Red XIII.jpg',
+    },    
+    {
+        name: 'Tifa',
+        img: 'images/Tifa.jpg',
+    },
+    {
+        name: 'Vincent',
+        img: 'images/Vincent.jpg',
+    },
+    {
+        name: 'Yuffie',
+        img: 'images/Yuffie.jpg',
+    },
+    {
+        name: 'Aerith',
+        img: 'images/Aerith.jpg',
+    },
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
