baaee4ff3f7a5658e81f202cb4f20eb708b2de86
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2023-12-15 01:43:21 -0500

    Added JS functions to make cards appear on web and console logs card data

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,14 +73,42 @@ const cardArray = [
         name: 'Aerith',
         img: 'images/Aerith.jpg',
     },
+    {
+        name: 'cardBack',
+        img: 'images/cardBack.png',
+    },
+    {
+        name: 'white',
+        img: 'images/white.png',
+    }
 ]
 
 cardArray.sort(() => 0.5 - Math.random())
 
+//--- Above is a shortcut to sort arrays randomly using .sort ---//
+
 const gridDisplay = document.querySelector('#grid')
+const cardChosen = []
 
 function createBoard() {
-    for (let i = 0; i < 19; i++) {
+    for (i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
+        card.setAttribute('src', 'images/backCardsmall.png');
+        card.setAttribute('data-id', i);
+        cardChosen.push()
+        card.addEventListener('click', flipCard)
+        gridDisplay.append(card)
+        // console.log(card, i)
     }
+}
+
+createBoard();
+
+function flipCard(){
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    cardChosen.push(cardArray[cardId].name)
+    console.log('clicked', cardId)
+    console.log(cardChosen)
+    this.setAttribute('src', cardArray[cardId].img)
 }
